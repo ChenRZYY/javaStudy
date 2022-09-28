41c3eb1f837888b901a212d651624f95fefc54ea
陈振东 <63147780@qq.com>
2022-09-28 22:26:13 +0800

    Delete java directory

--- java/配置文件/一键启动脚本/zookeeper_批量部署启动脚本/zkStart.sh
+++
@@ -1,8 +0,0 @@
-# node01 node02 node03 是三台 zkServer 节点，注意配置 hosts 文件
-for host in node01 node02 node03
-do
-	echo "===========start zk cluster :$host==============="
-	ssh  $host 'source /etc/profile; /export/servers/zookeeper-3.4.9/bin/zkServer.sh start'
-done
-# 休眠 2 秒
-# sleep 2s
